a90f369fa5a38a869efc4e917fabe648902e6623
souravt <sourav.tripathy@gmail.com>
2023-07-24 11:24:52 +0530

    Bootstrapping frontend porject

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+                
+        <p>
+          Send a Message
+        </p>
+
+        <p>
+          View Messages
+        </p>
+
+        <p>
+          Send a ping
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
